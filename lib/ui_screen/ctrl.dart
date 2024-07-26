0c8e7665e8934e503644fbd11b39fa7b8b7844c4
lunareraze <sapitridina88@gmail.com>
2024-07-26 15:03:54 +0700

    feat: final submit until it is shown on Home Page  as list

--- lib/ui_screen/ctrl.dart
+++ lib/ui_screen/ctrl.dart
@@ -3,3 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 Future<void> createDocument(Map<String, dynamic> data) async {
   await FirebaseFirestore.instance.collection('username').doc().set(data);
 }
+
+Future<QuerySnapshot<Map<String, dynamic>>> getColl() async {
+  final result = await FirebaseFirestore.instance.collection('username').get();
+
+  return result;
+}
--- lib/ui_screen/home.dart
+++ lib/ui_screen/home.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fs_practice/ui_screen/ctrl.dart';
 import 'package:fs_practice/ui_screen/input.dart';
 
 class HomeList extends StatelessWidget {
@@ -24,60 +24,27 @@ class HomeList extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            OutlinedButton(
-              onPressed: () async {
-                // await FirebaseFirestore.instance.collection('username').doc().set(
-                //   {
-                //     'id': 'this is id',
-                //     'nama': 'shane',
-                //     'umur': 22,
-                //   },
-                // );
-                // _ct.createDocument(
-                //     // {
-                //     //   // "id": "id from new created",
-                //     //   // "nama": "nama dari created",
-                //     //   // "umur": 22,
-                //     // },
-                //     );
-              },
-              child: const Text('C'),
-            ),
-            const SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () async {
-                final result = await FirebaseFirestore.instance.collection('username').get();
+            FutureBuilder(
+              future: getColl(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                      children: List.generate(
+                    snapshot.data!.docs.length,
+                    (index) => Card(
+                      child: ListTile(
+                        title: Text(snapshot.data!.docs[index].data()['nama']),
+                        // subtitle: Text(snapshot.data!.docs[index].data()['id'].toString()),
+                        subtitle: Text(snapshot.data!.docs[index].data()['id'].toString()),
+                      ),
+                    ),
+                  ));
+                }
 
-                debugPrint(result.toString());
-                debugPrint(result.docs.toString());
-                debugPrint(result.docs[0].data().toString());
-
-                //* this one under this is for when you included the docs on the function,
-                //* then you can access the data by running this dePrint below:
-                // debugPrint(result.data().toString());
+                return const Text('home');
               },
-              child: const Text('R'),
-            ),
-            const SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () async {
-                await FirebaseFirestore.instance.collection('username').doc('2eBaMc2LbsgA8cmSruM7').update({
-                  'id': "this is new id from update",
-                  "nama": "shane filan one",
-                  "umur": 24,
-                });
-              },
-              child: const Text('U'),
-            ),
-            const SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () async {
-                await FirebaseFirestore.instance.collection('username').doc('2eBaMc2LbsgA8cmSruM7').delete();
-              },
-              child: const Text('D'),
-            ),
+            )
           ],
         ),
       ),
